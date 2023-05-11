2d933a3657635ff77b83ea316c47ab649b9c3dc3
MamoruDS <mamoruds.io@gmail.com>
2023-05-11 12:07:26 +0900

    refactor: `_get_frames` -> `_get_frame`; `_get_gt_bboxes` -> `_get_gt_bbox`

--- libs/video.py
+++ libs/video.py
@@ -44,31 +44,31 @@ class Video(_Video[D]):
 
         return video
 
-    def _get_frames(self, idx: int) -> npt.NDArray[np.uint8]:
+    def _get_frame(self, idx: int) -> npt.NDArray[np.uint8]:
         # TODO: assume frames are jpeg compressed
         buf = np.frombuffer(self._frames[idx], dtype=np.uint8)
         return cv2.imdecode(buf, cv2.IMREAD_COLOR)
 
-    def _get_gt_bboxes(self, idx: int) -> npt.NDArray[np.float32]:
+    def _get_gt_bbox(self, idx: int) -> npt.NDArray[np.float32]:
         return self._gt_bboxes[idx]
 
     @property
     def init_bbox(self) -> npt.NDArray[np.float32]:
-        return self._get_gt_bboxes(0)
+        return self._get_gt_bbox(0)
 
     @property
     def width(self) -> int:
-        return self._get_frames(0).shape[1]
+        return self._get_frame(0).shape[1]
     
     @property
     def height(self) -> int:
-        return self._get_frames(0).shape[0]
+        return self._get_frame(0).shape[0]
 
     def __len__(self) -> int:
         return self._frames.shape[0]
 
     def __getitem__(self, idx: int) -> VideoItem:
-        return self._get_frames(idx), self._get_gt_bboxes(idx)
+        return self._get_frame(idx), self._get_gt_bbox(idx)
 
     def __iter__(self) -> Iterator[VideoItem]:
         for i in range(len(self)):
